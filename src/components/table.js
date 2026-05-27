da5ac4869daef5baac531c2d15faf538c8bc0a84
Earl of Tureyev <lider-1@mail.ru>
2026-05-27 16:39:42 +0500

    Extend table with before/after modules

--- src/components/table.js
+++ src/components/table.js
@@ -12,6 +12,24 @@ export function initTable(settings, onAction) {
   const root = cloneTemplate(tableTemplate);
 
   // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  const beforeReversed = [...before];
+
+  beforeReversed.reverse().forEach((subName) => {
+    console.log('Cloning template: "{subName}"');
+    const subTemplate = cloneTemplate(subName);
+    console.log("Result:", subTemplate);
+
+    root[subName] = cloneTemplate(subName);
+    root.container.prepend(root[subName].container);
+  });
+  after.forEach((subName) => {
+    console.log('Cloning tempLate: "${subName}"');
+    const subTemplate = cloneTemplate(subName);
+    console.log("Result:", subTemplate);
+
+    root[subName] = cloneTemplate(subName);
+    root.container.append(root[subName].container);
+  });
 
   // @todo: #1.3 —  обработать события и вызвать onAction()
 
