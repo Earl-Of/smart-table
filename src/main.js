c62dfa8b29eebf0baae13260bb7d78540940505e
Earl of Tureyev <lider-1@mail.ru>
2026-05-27 19:31:38 +0500

    Implement pagination

--- src/main.js
+++ src/main.js
@@ -1,54 +1,112 @@
-import './fonts/ys-display/fonts.css'
-import './style.css'
+import "./fonts/ys-display/fonts.css";
+import "./style.css";
 
-import {data as sourceData} from "./data/dataset_1.js";
+import { data as sourceData } from "./data/dataset_1.js";
 
-import {initData} from "./data.js";
-import {processFormData} from "./lib/utils.js";
+import { initData } from "./data.js";
+import { processFormData } from "./lib/utils.js";
 
-import {initTable} from "./components/table.js";
+import { initTable } from "./components/table.js";
 // @todo: подключение
 
-
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const api = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
  * @returns {Object}
  */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
+  const state = processFormData(new FormData(sampleTable.container));
+  const rowsPerPage = parseInt(state.rowsPerPage);
+  const page = parseInt(state.page ?? 1);
 
-    return {
-        ...state
-    };
+  return {
+    ...state,
+    rowsPerPage,
+    page,
+  };
 }
 
 /**
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+async function render(action) {
+  let state = collectState(); // состояние полей из таблицы
+  //let result = [...data]; // копируем для последующего изменения
+  let query = {};
+  // @todo: использование
+  // result = applySearching(result, state, action);
+  // result = applyFiltering(result, state, action);
+  // result = applySorting(result, state, action);
+  // result = applyPagination(result, state, action);
+  query = applyPagination(query, state, action);
+  query = applyFiltering(query, state, action);
+  query = applySearching(query, state, action);
+  query = applySorting(query, state, action);
 
+  const { total, items } = await api.getRecords(query);
 
-    sampleTable.render(result)
+  updatePagination(total, query);
+
+  sampleTable.render(items);
 }
 
-const sampleTable = initTable({
-    tableTemplate: 'table',
-    rowTemplate: 'row',
-    before: [],
-    after: []
-}, render);
+const sampleTable = initTable(
+  {
+    tableTemplate: "table",
+    rowTemplate: "row",
+    before: ["search", "header", "filter"],
+    after: ["pagination"],
+  },
+  render,
+);
 
 // @todo: инициализация
+import { initPagination } from "./components/pagination.js";
 
+// const {applyPagination, initPagination} = initPagination(...);
+const { applyPagination, updatePagination } = initPagination(
+  sampleTable.pagination.elements,
+  (el, page, isCurrent) => {
+    const input = el.querySelector("input");
+    const label = el.querySelector("span");
+    input.value = page;
+    input.checked = isCurrent;
+    label.textContent = page;
+    return el;
+  },
+);
 
-const appRoot = document.querySelector('#app');
+import { initSorting } from "./components/sorting.js";
+
+const applySorting = initSorting([
+  sampleTable.header.elements.sortByDate,
+  sampleTable.header.elements.sortByTotal,
+]);
+
+import { initFiltering } from "./components/filtering.js";
+
+const { applyFiltering, updateIndexes } = initFiltering(
+  sampleTable.filter.elements,
+);
+
+import { initSearching } from "./components/searching.js";
+
+// Передаём имя поля, где будет храниться строка поиска
+const applySearching = initSearching("search");
+
+const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+  const indexes = await api.getIndexes();
+
+  updateIndexes(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers,
+  });
+}
+
+// render();
+init().then(render);
